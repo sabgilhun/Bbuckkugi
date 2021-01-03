b9fbf6d122e5a37b1caed7c9c1b7f7a79fa6dc7e
sabgilhun <sabgilhun@gmail.com>
2021-01-04 01:58:18 +0900

    add permission check

--- app/src/main/java/com/sabgil/bbuckkugi/MainActivity.kt
+++ app/src/main/java/com/sabgil/bbuckkugi/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.sabgil.bbuckkugi
 
+import android.Manifest.permission.*
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.nearby.Nearby
 import com.google.android.gms.nearby.connection.*
 import java.nio.charset.Charset
@@ -87,6 +91,8 @@ class MainActivity : AppCompatActivity() {
             isHost = false
             startDiscovery()
         }
+
+        checkAndRequestPermission()
     }
 
     private fun startAdvertising() {
@@ -129,5 +135,69 @@ class MainActivity : AppCompatActivity() {
             else Payload.fromBytes(byteArrayOf("not host".toByte()))
 
         Nearby.getConnectionsClient(this).sendPayload(endpointId, bytesPayload)
+    }
+
+    private val needPermissionsUnderQ: Array<String> = arrayOf(
+        BLUETOOTH,
+        BLUETOOTH_ADMIN,
+        ACCESS_WIFI_STATE,
+        CHANGE_WIFI_STATE,
+        ACCESS_COARSE_LOCATION
+    )
+
+    private val needPermissionsOverQ: Array<String> = arrayOf(
+        BLUETOOTH,
+        BLUETOOTH_ADMIN,
+        ACCESS_WIFI_STATE,
+        CHANGE_WIFI_STATE,
+        ACCESS_FINE_LOCATION
+    )
+
+    private val needsPermissions =
+        if (Build.VERSION.SDK_INT < 29) needPermissionsUnderQ else needPermissionsOverQ
+
+    private fun hasPermissions(): Boolean {
+        for (permission in needsPermissions) {
+            if (checkSelfPermissionCompat(permission) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private val PERMISSION_REQUEST_CODE = 1001
+
+    private fun checkAndRequestPermission() {
+        if (!hasPermissions()) {
+            requestPermissionsCompat(needsPermissions, PERMISSION_REQUEST_CODE)
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            for (grantResult in grantResults) {
+                if (grantResult == PackageManager.PERMISSION_DENIED) {
+                    finish()
+                    return
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun AppCompatActivity.checkSelfPermissionCompat(permission: String) =
+        ActivityCompat.checkSelfPermission(this, permission)
+
+    private fun AppCompatActivity.shouldShowRequestPermissionRationaleCompat(permission: String) =
+        ActivityCompat.shouldShowRequestPermissionRationale(this, permission)
+
+    private fun AppCompatActivity.requestPermissionsCompat(
+        permissionsArray: Array<String>,
+        requestCode: Int
+    ) {
+        ActivityCompat.requestPermissions(this, permissionsArray, requestCode)
     }
 }
