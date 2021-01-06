ed12ba0a66c0d4f11ba696a445226d4d2bd19579
sabgilhun <sabgilhun@gmail.com>
2021-01-06 09:19:54 +0900

    add common ext code

---
+++ app/src/main/java/com/sabgil/bbuckkugi/common/ext/ActivityExt.kt
@@ -0,0 +1,19 @@
+package com.sabgil.bbuckkugi.common.ext
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.core.os.bundleOf
+import kotlin.reflect.KClass
+
+fun Context.startWith(target: KClass<out Activity>, vararg pairs: Pair<String, Any?>) {
+    startActivity(Intent(this, target.java).apply {
+        putExtras(bundleOf(*pairs))
+    })
+}
+
+fun Context.startOnTop(target: KClass<out Activity>) {
+    startActivity(Intent(this, target.java).apply {
+        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+    })
+}
---
+++ app/src/main/java/com/sabgil/bbuckkugi/common/ext/ContextExt.kt
@@ -0,0 +1,10 @@
+package com.sabgil.bbuckkugi.common.ext
+
+import android.content.Context
+import android.view.LayoutInflater
+
+val Context.layoutInflater get() = requireNotNull(LayoutInflater.from(this))
+
+fun Context.dpToPx(dp: Float) = dp * (resources.displayMetrics.densityDpi / 160f)
+
+fun Context.pxToDp(px: Float) = px / (resources.displayMetrics.densityDpi / 160f)
