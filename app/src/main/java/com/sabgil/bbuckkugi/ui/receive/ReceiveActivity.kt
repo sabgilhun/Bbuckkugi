d5c9e88b40b4930d9531dede861578988674540d
Yonghun Kim <sabgilhun@gmail.com>
2021-01-29 00:00:46 +0900

    add progress bar

--- app/src/main/java/com/sabgil/bbuckkugi/ui/receive/ReceiveActivity.kt
+++ app/src/main/java/com/sabgil/bbuckkugi/ui/receive/ReceiveActivity.kt
@@ -4,6 +4,7 @@ import android.app.TaskStackBuilder
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.core.content.ContextCompat
 import com.sabgil.bbuckkugi.R
 import com.sabgil.bbuckkugi.base.BaseActivity
@@ -11,9 +12,9 @@ import com.sabgil.bbuckkugi.common.Data
 import com.sabgil.bbuckkugi.databinding.ActivityReceiveBinding
 import com.sabgil.bbuckkugi.model.Message
 import com.sabgil.bbuckkugi.service.channel.CommunicationChannel
-import com.sabgil.bbuckkugi.ui.send.SendActivity
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class ReceiveActivity : BaseActivity<ActivityReceiveBinding>(R.layout.activity_receive) {
@@ -39,6 +40,7 @@ class ReceiveActivity : BaseActivity<ActivityReceiveBinding>(R.layout.activity_r
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupChannel()
+        startTimer()
     }
 
     private fun setupChannel() {
@@ -57,7 +59,23 @@ class ReceiveActivity : BaseActivity<ActivityReceiveBinding>(R.layout.activity_r
         }
     }
 
+    private fun startTimer() {
+        object : CountDownTimer(TIMER_FINISH_DELAY, COUNT_DOWN_INTERVAL) {
+            override fun onTick(millisUntilFinished: Long) {
+                binding.progressBar.progress =
+                    ((TIMER_FINISH_DELAY - millisUntilFinished) / COUNT_DOWN_INTERVAL).toInt()
+            }
+
+            override fun onFinish() {
+                finish()
+            }
+        }.start()
+    }
+
     companion object {
+        private const val COUNT_DOWN_INTERVAL = 1000L
+        private const val TIMER_FINISH_DELAY = 5 * 60 * COUNT_DOWN_INTERVAL
+
         fun startOnHome(context: Context) {
             TaskStackBuilder.create(context)
                 .addNextIntentWithParentStack(Intent(context, ReceiveActivity::class.java))
