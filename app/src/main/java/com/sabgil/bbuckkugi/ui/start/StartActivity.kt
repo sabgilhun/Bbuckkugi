346c1f734837067c8798721e040ff45a2c63d963
Yonghun Kim <sabgilhun@gmail.com>
2021-02-09 22:17:20 +0900

    implement StartActivity ui

---
+++ app/src/main/java/com/sabgil/bbuckkugi/ui/start/StartActivity.kt
@@ -0,0 +1,10 @@
+package com.sabgil.bbuckkugi.ui.start
+
+import com.sabgil.bbuckkugi.R
+import com.sabgil.bbuckkugi.base.BaseActivity
+import com.sabgil.bbuckkugi.databinding.ActivityStartBinding
+
+class StartActivity : BaseActivity<ActivityStartBinding>(R.layout.activity_start) {
+
+    
+}
